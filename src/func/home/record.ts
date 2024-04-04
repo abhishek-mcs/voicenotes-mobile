700cf7af5f2f13209bc6ac362821eef37bb87ffe
athulmohan1 <athul@buymeacoffee.com>
2024-04-04 13:02:45 +0530

    rec fun update

--- src/func/home/record.ts
+++ src/func/home/record.ts
@@ -41,7 +41,7 @@ export const onRecord = async (
         );
         setRec(recordingObject);
         setRecEnabled(true);
-      } else if (status.canAskAgain && status.status == "undetermined") {
+      } else if (status.canAskAgain && status.status == "undetermined"||status.status == "denied") {
         await Audio.requestPermissionsAsync().then(
           async ({ canAskAgain, status }) => {
             if (status == "granted") {
