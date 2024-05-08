3e9a657eee85398dfd2a0979d901460c33e350a1
athulmohan1 <athul@buymeacoffee.com>
2024-05-08 08:56:00 +0530

    IAP UI improvements

--- src/app/premium/index.tsx
+++ src/app/premium/index.tsx
@@ -41,7 +41,7 @@ export default (props:any) => {
 
   return (
     <SafeAreaView style={styles.main}>
-    <Touchable style={{position:'absolute',padding:10,right:10,top:30,zIndex:10}} onPress={()=>router?.back()}>
+    <Touchable style={{position:'absolute',padding:10,right:8,top:8,zIndex:10}} onPress={()=>router?.back()}>
       <SvgXml xml={settingsSvg.close}/>
     </Touchable>
       <View style={styles.container}>
@@ -52,12 +52,12 @@ export default (props:any) => {
             <SvgXml xml={iapSvg.done}/>
             <Text style={styles.desc}>Limitless recording, instead of 1 min/note</Text>
           </View>
-          <View style={styles.descView}>
+          <View style={[styles.descView,{marginBottom:30}]}>
             <SvgXml xml={iapSvg.done}/>
             <Text style={styles.desc}>Smartest AI models (GPT-4 Turbo, Claude Opus)</Text>
           </View>
-          <Btn type="monthly" price={pack[1]?.product?.priceString||'$10.00'} selected={selected=='monthly'} onPress={()=>setSelected('monthly')} underlay="#f9f9f9" title="Monthly"/>
           <Btn type="believer" price={pack[0]?.product?.priceString||'$50.00'} selected={selected=='believer'} onPress={()=>setSelected('believer')} underlay="#f9f9f9" title="Believer"/>
+          <Btn type="monthly" price={pack[1]?.product?.priceString||'$10.00'} selected={selected=='monthly'} onPress={()=>setSelected('monthly')} underlay="#f9f9f9" title="Monthly"/>
           <Btn type="upgrade" onPress={onUpgrade} underlay={Colors.primaryWithOpacity(0.8)} title="Upgrade" isLoading={isLoading}/>
         </View>
       </View>
@@ -66,7 +66,7 @@ export default (props:any) => {
 }
 
 const Btn = ({title,type,price,onPress,underlay,selected=false,isLoading=false}:Props) =>
-  <TouchableHighlight onPress={onPress} style={[styles.btn,styles.border,type=="upgrade"?styles.btnFilled:selected?{borderColor:Colors.primary}:{}]} underlayColor={underlay}>
+  <TouchableHighlight onPress={onPress} style={[styles.btn,styles.border,type=="upgrade"?styles.btnFilled:selected?{borderColor:Colors.primary,borderWidth:2}:{}]} underlayColor={underlay}>
     {(type=='monthly'||type=='believer')?
     <><View>
         <Text style={styles.btnText}>{title}</Text>
@@ -88,18 +88,18 @@ const styles = StyleSheet.create({
   main:{flex:1,backgroundColor:'#fff'},
   container:{flex:1},
   subContainer:{flex:2,padding:16},
-  img:{flex:1,width:'80%',height:screenHeight/2,alignSelf:'center'},
-  title:{fontSize:30,fontFamily:'Primary-Medium',color:'#222',marginBottom:24},
-  descView:{flexDirection:'row',alignItems:'flex-start',marginBottom:12},
-  desc:{marginLeft:9,fontSize:16,fontFamily:'Primary-Regular',color:'#222',lineHeight:22},
-  border:{borderWidth:1,borderColor:Colors.greyWithOpacity(0.5)},
+  img:{width:'80%',height:screenHeight/3,alignSelf:'center',marginTop:20},
+  title:{fontSize:20,fontFamily:'Primary-Semibold',color:'#222',marginBottom:20,alignSelf:'center'},
+  descView:{flexDirection:'row',alignItems:'flex-start',paddingHorizontal:20,marginBottom:17},
+  desc:{marginLeft:9,fontSize:14,fontFamily:'Primary-Regular',color:'#222',lineHeight:22,marginTop:-4},
+  border:{borderWidth:1,borderColor:'rgba(229, 229, 229, 0.9)'},
   btnFilled:{height:56,width:'100%',backgroundColor:Colors.primary,justifyContent:'center',marginVertical:20},
-  btn:{minHeight:60,width:'100%',paddingVertical:8,justifyContent:'space-between',alignItems:'center',flexDirection:'row',paddingHorizontal:16,marginTop:16,backgroundColor:'#fff',borderRadius:8},
+  btn:{minHeight:48,width:'100%',paddingVertical:8,justifyContent:'space-between',alignItems:'center',flexDirection:'row',paddingHorizontal:16,marginTop:16,backgroundColor:'#fff',borderRadius:8},
   btnContent:{marginTop:4,flexDirection:'row',alignItems:'center'},
-  btnText:{fontSize:16,fontFamily:'Primary-Medium',color:'#222'},
-  offer:{color:'#FF4538', fontFamily:'Primary-Regular',fontSize:12,textAlignVertical:'center',marginLeft:4},
-  btnPrice:{fontSize:16,fontFamily:'Primary-Medium',color:'#222'},
-  btnPriceType:{color:Colors.grey,fontSize:14,fontFamily:'Primary-Regular',marginLeft:4},
+  btnText:{fontSize:14,fontFamily:'Primary-Medium',color:'#222'},
+  offer:{color:'#FF4538', fontFamily:'Primary-Semibold',fontSize:10,textAlignVertical:'center',marginLeft:4},
+  btnPrice:{fontSize:14,fontFamily:'Primary-Medium',color:'#222'},
+  btnPriceType:{color:Colors.grey,fontSize:14,fontFamily:'Primary-Medium',marginLeft:4},
 })
 
 interface Props {
