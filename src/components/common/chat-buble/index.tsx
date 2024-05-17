e298a830534bc16fbc54cfc2a5f750088d05dd9c
athulmohan1 <athul@buymeacoffee.com>
2024-05-17 11:19:43 +0530

    transcript fix

--- src/components/common/chat-buble/index.tsx
+++ src/components/common/chat-buble/index.tsx
@@ -1,9 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import { View, Text, TextStyle } from 'react-native';
-import { formatHtmlText } from 'utils/common';
+import { formatHtmlText, screenWidth } from 'utils/common';
+import {RenderHTML} from 'react-native-render-html';
 
 export default ({ delay=30,message,style={},triggerAnimation=0,disableGenerating=()=>{},continueGenerating=true }:{delay?:number,message:string,style:TextStyle,triggerAnimation:number,disableGenerating:()=>void,continueGenerating?:boolean})=> {
     const [displayedMessage, setDisplayedMessage]:any = useState('');
+
+    const containsHTML = (str:string) => {
+      const htmlPattern = /<[^>]+>/g;
+      return htmlPattern.test(str);
+    };
 
     useEffect(() => {
       let currentIndex = 0;
@@ -26,24 +32,18 @@ export default ({ delay=30,message,style={},triggerAnimation=0,disableGenerating
   
       return () => clearInterval(interval);
     }, [message, triggerAnimation,continueGenerating]);
-
+    console.log('displayedMessage',containsHTML(displayedMessage))
+  if(containsHTML(displayedMessage)){
+    return(
+      <View style={{marginTop:8}}>
+        <RenderHTML
+          contentWidth={screenWidth-32}
+          source={{html:displayedMessage}}
+        />
+      </View>
+    )
+  }
   return (
-      <HTMLText style={[style,{}]}>{displayedMessage}</HTMLText>
-  );
-};
-
-const HTMLText = ({ children ,style={}}:any) => {
-  const htmlRegex = /<([^>]+)>/g;
-  let formattedText = children.replaceAll(/<br\s*\/?>/gi, '\n');
-  formattedText = formattedText.replaceAll(/&nbsp;|&#160;/gi, ' ');
-  formattedText = formattedText.replace(/<i>(.*?)<\/i>/g, (match:any, content:any) => {
-    return <Text style={{ fontStyle: 'italic' }}>{content}</Text>;
-  });
-  formattedText = formattedText.replace(/<b>(.*?)<\/b>/g, (match:any, content:any) => {
-    return <Text style={{ fontWeight: 'bold' }}>{content}</Text>;
-  });
-  formattedText = formattedText.replaceAll(htmlRegex, '');
-  return (
-      <Text style={style}>{formattedText}</Text>
+      <Text style={[style,{}]}>{displayedMessage}</Text>
   );
 };
