07023f9b967c94679ad8f82487468ff724cfc4a6
athulmohan1 <athul@buymeacoffee.com>
2024-06-12 12:33:39 +0530

    firebase integration

--- src/func/firebase/listen-ai-create.ts
+++ src/func/firebase/listen-ai-create.ts
@@ -2,15 +2,11 @@ import { ref, onValue, Database, remove } from "firebase/database";
 import { db } from "../../../firebaseConfig";
 export default async({ id = null, getCreation = async(v: any) => {} }) => {
   const statusRef = ref(db, "processStatuses/aicreate/" + id);
-  console.log("AI CREATE STATUS REF", statusRef);
   onValue(statusRef, async (snapshot) => {
-    console.log("AI CREATE STATUS", id,snapshot.exists());
     if (snapshot.exists()) {
       const status = snapshot.val();
-      console.log("AI CREATE STATUS", status);
       if (status === 1) {
         await getCreation(id);
-        console.log("AI CREATE DONE");
         remove(statusRef);
       }
     }
