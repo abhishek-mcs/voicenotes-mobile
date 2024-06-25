5d8abc976fdfd24be5a0aa24c86158315abea344
athulmohan1 <athul@buymeacoffee.com>
2024-06-25 11:26:13 +0530

    creation flow points fix

--- src/components/home/ai-created-view.tsx
+++ src/components/home/ai-created-view.tsx
@@ -50,10 +50,10 @@ export default ({content,date=undefined,type="Summary",id}:{content:any,date:any
       </View>
       {(type=="summary"||type=="tweet"||type=="custom")?<Text style={titleStyle} numberOfLines={expand?1000:1}>{content}</Text>
       :(type=="points"||type=="todo")?
-      <Text numberOfLines={expand?1000:1} style={{marginTop:6}}>{content?.map((itm:string,i:number)=><Text key={i} style={titleStyle}>{`${type=="points"?'\u2022 ':i+1+'. '} ${itm}${content?.length-1==i?'':'\n'}`}</Text>)}</Text>
+      <Text numberOfLines={expand?1000:1} style={{marginTop:6}}>{(!!content&&content?.length>0)&&content.map((itm:string,i:number)=><Text key={i} style={titleStyle}>{`${type=="points"?'\u2022 ':i+1+'. '} ${itm}${content?.length-1==i?'':'\n'}`}</Text>)}</Text>
       :type=="blog"?
       <Text numberOfLines={expand?1000:1} style={{marginTop:6}}>
-      {!!content&&content?.map((itm:string,i:number)=>
+      {(!!content&&content?.length>0)&&content?.map((itm:string,i:number)=>
           <Text key={i} style={titleStyle}>{itm}</Text>
       )}</Text>
       :<Text numberOfLines={expand?1000:1} style={{marginTop:6}}>
