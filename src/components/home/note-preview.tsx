b6494d96a4ef8d9d558473c7b579eb24151b0750
Aldrin Jenson <aldrin@voicenotes.com>
2024-07-08 13:18:54 +0530

    Add extra left margin for Delete icon

--- src/components/home/note-preview.tsx
+++ src/components/home/note-preview.tsx
@@ -480,7 +480,7 @@ export default forwardRef(({
                           </MenuItem>
                           {!!token && <MenuItem style={styles.menuItem} onPress={onDelete}>
                             <View style={styles.row}>
-                              <SvgXml xml={home.delete} />
+                              <SvgXml xml={home.delete} style={{marginLeft: 2}} />
                               <Text style={styles.menuItemTxt}>Delete</Text>
                             </View>
                           </MenuItem>}
