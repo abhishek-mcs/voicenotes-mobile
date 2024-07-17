8a5b7a686a0320c30e82374410cd3549f649c2bc
athulmohan1 <athul@buymeacoffee.com>
2024-07-17 13:50:44 +0530

    loader issue fix

--- src/components/home/note-preview.tsx
+++ src/components/home/note-preview.tsx
@@ -158,8 +158,7 @@ export default forwardRef(({
     } else {
       console.warn('Retry transcript')
       setTranscriptLoading(true)
-      note.title=null
-      note.transcript= ''
+      // dispatch()
       await addTranscript.mutateAsync(note?.id, {
         onSuccess: async () => {
           setTranscriptLoading(false)
