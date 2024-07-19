25ce7c640721e7ef2713b053ce162ad34ff0222d
athulmohan1 <athul@buymeacoffee.com>
2024-07-19 12:18:33 +0530

    api update

--- src/services/api/api-constants.ts
+++ src/services/api/api-constants.ts
@@ -81,7 +81,7 @@ const ENVURLSet: ENVURLs = {
 //     ? Environment.stage
 //     : Environment.production
 
-const currentENV = Environment.stage;
+const currentENV = Environment.production;
 const ota=".0"
 const currentVersion = Application.nativeApplicationVersion+ota
 
