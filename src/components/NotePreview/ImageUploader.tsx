e11ecc3e883aa4ec5076ecc2ce676d8135442564
Aldrin Jenson <aldrin@voicenotes.com>
2024-07-22 10:48:42 +0530

    Remove cropping

--- src/components/NotePreview/ImageUploader.tsx
+++ src/components/NotePreview/ImageUploader.tsx
@@ -102,7 +102,7 @@ const ImageUploader = ({
     let result = await launch({
       mediaTypes: ImagePicker.MediaTypeOptions.Images,
       allowsEditing: true,
-      // aspect: [4, 3],
+      aspect: [4, 3],
       quality: 1,
     });
 
