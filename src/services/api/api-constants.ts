0bc50579efac9c71c092bc2d9f417cf42fc3c7f5
athulmohan1 <athul@buymeacoffee.com>
2024-07-30 17:20:45 +0530

    auto split updated to 20 minutes

--- src/services/api/api-constants.ts
+++ src/services/api/api-constants.ts
@@ -82,7 +82,7 @@ const ENVURLSet: ENVURLs = {
 //     : Environment.production
 
 const currentENV = Environment.production;
-const ota=".3"
+const ota=".4"
 const currentVersion = Application.nativeApplicationVersion+ota
 
 const MAIN_URL = ENVURLSet[currentENV].MAIN_URL
