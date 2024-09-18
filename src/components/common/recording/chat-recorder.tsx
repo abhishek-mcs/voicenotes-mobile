f4b088fa62605139397e41c28f21fc525324b9d3
athulmohan1 <athul@buymeacoffee.com>
2024-09-18 10:14:30 +0530

    recorder reverted

--- src/components/common/recording/chat-recorder.tsx
+++ src/components/common/recording/chat-recorder.tsx
@@ -4,35 +4,26 @@ import { StyleSheet } from "react-native"
 import { Text } from "react-native"
 import { View } from "react-native"
 import RecButton from "./rec-button"
-import { AIModalSVG } from "assets/svg/AIModalSvg"
-import Waveform from "./waveform"
-import { screenWidth } from "utils/common"
-import { memo } from "react"
 
-const ChatRecorder = ({onPause,onStopRecord=(v:any)=>{},duration=0,totalDuration='',onCancel=()=>{},recording=null}:any)=>{
+export default ({onPause,onStopRecord=(v:any)=>{},duration=0,totalDuration='',onCancel=()=>{}}:any)=>{
   const formattedDuration = new Date(duration).toISOString().substring(14, 19);
-console.log('rec')
+
     return (
-      <View style={{height:97,paddingTop:10}}>
-        <View style={[styles.row,{alignSelf:'center'}]}>
-          <View style={{backgroundColor:'red',height:6,width:6,borderRadius:10,marginRight:8}}/>
-          <Text style={styles.tabItemText}>{`${formattedDuration}${totalDuration}`}</Text>
-        </View>
-        <View style={[{justifyContent:'space-between',flexDirection:'row',flex:1,alignItems:'center'},onPause?{alignItems:'flex-end'}:{}]}>
-          <RecButton title="" bgColor="transparent" icon={AIModalSVG.cancel} underlayColor="transparent" color={'#FF4538'} onPress={onCancel} style={{paddingHorizontal:0}}/>
-          
-          <Waveform recording={recording}/>
-          
+        <View style={[{justifyContent:'space-between',flexDirection:'row',flex:1,alignItems:'center'},onPause?{alignItems:'flex-end',height:156}:{}]}>
+          <RecButton title="Cancel" bgColor="#FF45380D" underlayColor="#FF45380F" color={'#FF4538'} onPress={onCancel} style={{paddingHorizontal:20}}/>
+          <View style={[styles.row]}>
+            <View style={{backgroundColor:'red',height:6,width:6,borderRadius:10,marginRight:8}}/>
+            <Text style={styles.tabItemText}>{`${formattedDuration}${totalDuration}`}</Text>
+          </View>
           <RecButton
-            title=""
-            icon={AIModalSVG.send}
+            title="Done"
+            icon={bottomSvg.done}
             color={Colors.green}
-            bgColor={'transparent'}
+            bgColor={Colors.greenWithOpacity(0.2)}
             underlayColor={Colors.greenWithOpacity(0.3)}
             onPress={onStopRecord}
-            style={{paddingHorizontal:0}}
+            style={{paddingHorizontal:20}}
           />
-        </View>
         </View>
     )
 }
@@ -48,5 +39,3 @@ const styles = StyleSheet.create({
     },
     row:{flexDirection:'row',alignItems:"center"},
   });
-
-  export default memo(ChatRecorder)
