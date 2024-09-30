3c3d628c8e60e7fd7d51fc3ddc2589c9a7e6b083
Andriy Hrytsyshyn <andrii.hrytsyshyn@kevychsolutions.com>
2024-09-30 13:45:44 +0300

    fix complication image

--- ios/Voicenotes Watch App/ComplicationController.swift
+++ ios/Voicenotes Watch App/ComplicationController.swift
@@ -13,7 +13,7 @@ final class ComplicationController: NSObject, CLKComplicationDataSource {
   func complicationDescriptors() async -> [CLKComplicationDescriptor] {
     [
       CLKComplicationDescriptor(
-        identifier: "watch-voicenote-app", // This id should be unique and stable.
+        identifier: "watch-voicenote-app-complication", // This id should be unique and stable.
         displayName: "Voicenote",
         supportedFamilies: [.graphicCircular, .graphicCorner])
     ]
@@ -21,7 +21,7 @@ final class ComplicationController: NSObject, CLKComplicationDataSource {
   
   // This method is for creating a complication sample. It defines how it will look in Complication Picker Mode.
   func localizableSampleTemplate(for complication: CLKComplication) async -> CLKComplicationTemplate? {
-    guard let fullColorImage = UIImage(named: "AppIcon") else { return nil }
+    guard let fullColorImage = UIImage(named: "complication_icon") else { return nil }
     let fullColorImageProvider = CLKFullColorImageProvider(fullColorImage: fullColorImage)
 
     switch complication.family {
@@ -41,8 +41,8 @@ final class ComplicationController: NSObject, CLKComplicationDataSource {
   // This method is for creating an actual live complication.
   func currentTimelineEntry(for complication: CLKComplication) async -> CLKComplicationTimelineEntry? {
     guard
-      let fullColorImage = UIImage(named: "AppIcon"),
-      let tintColorImage = UIImage(named: "AppIcon")
+      let fullColorImage = UIImage(named: "complication_icon"),
+      let tintColorImage = UIImage(named: "complication_icon_tinted")
     else { return nil }
     
     let tintColorImageProvider = CLKImageProvider(onePieceImage: tintColorImage)
