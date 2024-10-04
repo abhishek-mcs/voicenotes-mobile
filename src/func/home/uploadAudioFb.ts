303c08933458a0eb0bbbc99083163362e32f745f
athulmohan1 <athul@buymeacoffee.com>
2024-10-04 18:20:32 +0530

    done

--- src/func/home/uploadAudioFb.ts
+++ src/func/home/uploadAudioFb.ts
@@ -34,9 +34,9 @@ export const saveVoiceNote = async (data: {
   const filename = uri.split("/").pop();
 
   try {
-    const signedURLCall = await axiosApi.get("recordings/signed-url")
-    const signedURL=signedURLCall?.data?.url
-    const upload_id=signedURLCall?.data?.upload_id
+    // const signedURLCall = await axiosApi.get("recordings/signed-url")
+    // const signedURL=signedURLCall?.data?.url
+    // const upload_id=signedURLCall?.data?.upload_id
     const formData:any = new FormData();
     formData.append("audio", {
       uri: uri,
@@ -44,30 +44,30 @@ export const saveVoiceNote = async (data: {
       type: `audio/${filetype}`,
     });
 
-    const base64 = await FileSystem.readAsStringAsync(uri, {
-      encoding: FileSystem.EncodingType.Base64,
-    });
+    // const base64 = await FileSystem.readAsStringAsync(uri, {
+    //   encoding: FileSystem.EncodingType.Base64,
+    // });
 
-    const buffer = Buffer.from(base64, "base64");
-    // Make the POST request using axios
-    await axios.put(signedURL, buffer, {
-      headers: {
-        'Content-Type': `audio/${filetype}`,   // Ensure to set the correct MIME type
-      },
-    });
-    const formData1:any = new FormData();
-    formData1.append("upload_id", upload_id);
-    formData1.append("recording_identifier", temp_id);
+    // const buffer = Buffer.from(base64, "base64");
+    // // Make the POST request using axios
+    // await axios.put(signedURL, buffer, {
+    //   headers: {
+    //     'Content-Type': `audio/${filetype}`,   // Ensure to set the correct MIME type
+    //   },
+    // });
+    // const formData1:any = new FormData();
+    // formData1.append("upload_id", upload_id);
+    // formData1.append("recording_identifier", temp_id);
     if (parent_id) {
       console.log("appending parent id: ", parent_id);
-      formData1.append("parent_id", parent_id);
+      formData.append("parent_id", parent_id);
     }
-    formData1.append("duration", duration.toString());
-    formData1.append("device_info", JSON.stringify(deviceInfo));
-    formData1.append("recorded_at", recorded_at.toString());
+    formData.append("duration", duration.toString());
+    formData.append("device_info", JSON.stringify(deviceInfo));
+    formData.append("recorded_at", recorded_at.toString());
 
     // Make the POST request using axios
-    const response = await axiosApi.post("recordings/new", formData1, {
+    const response = await axiosApi.post("recordings/new", formData, {
       headers: {
         "Content-Type": "multipart/form-data",
       },
