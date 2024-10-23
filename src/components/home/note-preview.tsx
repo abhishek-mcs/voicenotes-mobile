4b981025c9dc5ca5887bc3606fb46e918ec951e5
vishaldsbmc <vishal@voicenotes.com>
2024-10-23 10:10:44 +0530

    increase more button size

--- src/components/home/note-preview.tsx
+++ src/components/home/note-preview.tsx
@@ -987,7 +987,7 @@ const NotePreview = forwardRef(
                   <Text style={{fontFamily:'Primary-Semibold',fontSize:14,color:Colors.blackWithOpacity(1),marginLeft:6}}>{formattedDuration}</Text>
                 </Touchable>
                 {(note?.status=="processed"||isSingle)&&
-                  <MoreOptions options={options} style={{height:29,paddingHorizontal:12,marginRight:-12,justifyContent:"center",alignItems:'center'}}>
+                  <MoreOptions options={options} style={{height:30,paddingHorizontal:15, paddingLeft: 30, marginRight:-12,justifyContent:"center",alignItems:'center'}}>
                   <SvgXml xml={home.moreNew}/>
                 </MoreOptions>}
                 </View>
