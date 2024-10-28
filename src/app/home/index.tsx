c2d2f40ee74539aca16900307db5c310b7a843ae
athulmohan1 <athul@buymeacoffee.com>
2024-10-28 13:47:30 +0530

    test

--- src/app/home/index.tsx
+++ src/app/home/index.tsx
@@ -303,10 +303,10 @@ export default () => {
     }
 
   useEffect(() => {
-    const tokenSubscription = actionEmitter.addListener('sendToken', () => {
-      console.log("React Native: Send token started");
-      NativeModules.TokenBridge.sendTokenToWatch(token);
-    });
+    // const tokenSubscription = actionEmitter.addListener('sendToken', () => {
+    //   console.log("React Native: Send token started");
+    //   NativeModules.TokenBridge.sendTokenToWatch(token);
+    // });
     const startRecordSubscription = actionEmitter.addListener('onStartRecord', () => {
       console.log("React Native: Recording started");
       setTimeout(() => {
