d410d579027286649810da68a451fc7ca0cd3bdc
athulmohan1 <athul@buymeacoffee.com>
2024-11-18 14:08:00 +0530

    menu android fix for foldable device

--- src/components/home/note-preview.tsx
+++ src/components/home/note-preview.tsx
@@ -1003,7 +1003,7 @@ const NotePreview = forwardRef(
                 } */}
                 </View>
                 {(note?.status=="processed"||isSingle||(isSubnote&&note?.transcript))&&
-                  <MoreOptions options={options} style={{height:30,width:30,zIndex:1000,position:'relative'}}>
+                  <MoreOptions options={options} style={{height:30,width:30,position:'relative'}}>
                     <View style={{height:30,width:30,zIndex:1000,borderRadius:100,backgroundColor:Colors.darkWithOpacity(0.05),justifyContent:"center",alignItems:'center'}}>
                   <SvgXml xml={home.moreNew}/>
                   </View>
