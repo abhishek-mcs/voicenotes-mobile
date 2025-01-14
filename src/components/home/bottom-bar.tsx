404d3b4b7f5177e8d391fb36c84e7da322721fd6
vishaldsbmc <vishal@voicenotes.com>
2025-01-14 08:09:37 +0530

    corrected time limit

--- src/components/home/bottom-bar.tsx
+++ src/components/home/bottom-bar.tsx
@@ -73,7 +73,7 @@ useEffect(() => {
           ) {
             onStopRecord(newDuration);
             return 0;
-          } else if (newDuration >= 120000 && !!token) {
+          } else if (newDuration >= 2400000 && !!token) {
             onStopRecord(newDuration, userDetails?.subscription_status);
             return 0;
           }
