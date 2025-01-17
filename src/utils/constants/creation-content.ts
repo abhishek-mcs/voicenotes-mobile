8db973f9606de07af62c519307f94db88330592a
vishaldsbmc <vishal@voicenotes.com>
2025-01-17 15:41:44 +0530

    added copy for loader

--- src/utils/constants/creation-content.ts
+++ src/utils/constants/creation-content.ts
@@ -5,6 +5,7 @@ const creationContent: any = {
     tweet:"Creating a tweet from your note",
     blog:"Creating a blog post from your note",
     email:"Creating an email from your note",
-    tidy:"Creating a cleaned-up version of your note"
+    tidy:"Creating a cleaned-up version of your note",
+    translate:"Translating your note"
 }
 export default creationContent;
