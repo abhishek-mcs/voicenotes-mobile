8bb4271f22395b36ba5210e2e2db09e1db0979a9
vishaldsbmc <vishal@voicenotes.com>
2025-03-03 17:01:19 +0530

    android: disabled notifications

--- src/app/settings/index.tsx
+++ src/app/settings/index.tsx
@@ -316,7 +316,7 @@ const Settings = () => {
               { title: 'Theme', data: [['auto', 'Auto', isIOS ? 'circle.lefthalf.fill' : 'circle-half-full'], ['light', 'Day', isIOS ? 'sun.max' : 'white-balance-sunny'], ['dark', 'Night', isIOS ? 'moon.zzz' : 'weather-night']], value: selectedTheme[theme], onPressMenu: onSelectTheme, isMenu: true },
               { title: 'Language', isMenu: true, data: Object.entries(languages), value: lang, onPressMenu: onSelectLang },
               { title: 'Names to remember', value: '', onPress: () => showScreen('names'), rightIcon: settingsSvg.arrow },
-              { title: 'Notifications', value: '', onPress: () => router.push('/settings/reminders'), rightIcon: settingsSvg.arrow }
+              // { title: 'Notifications', value: '', onPress: () => router.push('/settings/reminders'), rightIcon: settingsSvg.arrow }
             ]}
           />
           <Grouped
