08f44b6395a40b7e25eee773ceac3f408c1cf87a
vishaldsbmc <vishal@voicenotes.com>
2025-03-14 09:58:41 +0530

    decreased border width

--- src/components/home/Calendar.tsx
+++ src/components/home/Calendar.tsx
@@ -797,7 +797,7 @@ const useStyles = () => {
       width: CALENDAR_WIDTH,
       backgroundColor: Colors.grey2,
       borderRadius: 25,
-      borderWidth: 0.5,
+      borderWidth: 0.2,
       padding: 16,
       shadowColor: '#000',
       shadowOffset: { width: 4, height: 4 },
