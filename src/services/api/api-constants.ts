a698677b4ea6508bcadc3801073fdc9be04aa85f
vishaldsbmc <vishal@voicenotes.com>
2025-03-18 13:58:30 +0530

    switch to production api

--- src/services/api/api-constants.ts
+++ src/services/api/api-constants.ts
@@ -61,8 +61,8 @@ const ENVURLSet: ENVURLs = {
 //     ? Environment.stage
 //     : Environment.production
 
-const currentENV = Environment.stage;
-const ota=".24"
+const currentENV = Environment.production;
+const ota=".28"
 const currentVersion = Application.nativeApplicationVersion+ota
 
 
