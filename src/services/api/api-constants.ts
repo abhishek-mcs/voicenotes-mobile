2b78001647a1349f721ed115a0791d40bef3944d
vishaldsbmc <vishal@voicenotes.com>
2025-03-18 15:37:37 +0530

    bump version

--- src/services/api/api-constants.ts
+++ src/services/api/api-constants.ts
@@ -62,7 +62,7 @@ const ENVURLSet: ENVURLs = {
 //     : Environment.production
 
 const currentENV = Environment.production;
-const ota=".28"
+const ota=".29"
 const currentVersion = Application.nativeApplicationVersion+ota
 
 
