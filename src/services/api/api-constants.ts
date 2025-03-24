22b42b39fb8d3d56c580e4d0bf54063fe64cd65a
Athul Mohan <athul@buymeacoffee.com>
2025-03-24 15:02:58 +0530

    fix for meeting edit

--- src/services/api/api-constants.ts
+++ src/services/api/api-constants.ts
@@ -62,7 +62,7 @@ const ENVURLSet: ENVURLs = {
 //     : Environment.production
 
 const currentENV = Environment.production;
-const ota=".34"
+const ota=".35"
 const currentVersion = Application.nativeApplicationVersion+ota
 
 
