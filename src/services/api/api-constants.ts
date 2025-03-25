d4c226c9c99a225ed088e5b63116db592a5ab862
Merina <merina@Merinas-MacBook-Air.local>
2025-03-25 11:59:48 +0530

    production env

--- src/services/api/api-constants.ts
+++ src/services/api/api-constants.ts
@@ -61,8 +61,8 @@ const ENVURLSet: ENVURLs = {
 //     ? Environment.stage
 //     : Environment.production
 
-const currentENV = Environment.stage;
-const ota=".7"
+const currentENV = Environment.production;
+const ota=".1"
 const currentVersion = Application.nativeApplicationVersion+ota
 
 
