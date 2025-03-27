2c659c26440bdd1f585c8192de0c454bb99b1a38
Merina <merina@Merinas-MacBook-Air.local>
2025-03-27 10:07:17 +0530

    isbeliever check

--- src/components/home/header.tsx
+++ src/components/home/header.tsx
@@ -57,7 +57,7 @@ const Header = ({isLogged=true,isOffline,onCalendarToggled,scrollY,settingsRef,c
     route.navigate("/settings/")
   }
 
-  const isBeliever=(isTempIAPPurchased ?? userDetails?.subscription_status)
+  const isBeliever=(userDetails?.subscription_status||isTempIAPPurchased)
   const headerHeight=90
   const minHeaderHeight=40
   const titleFontSize=36
