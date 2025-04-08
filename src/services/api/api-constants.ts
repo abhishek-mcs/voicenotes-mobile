76a6328f36f02f20c7de8bd3d9049800bf17ba3e
Merina <merina@Merinas-MacBook-Air.local>
2025-04-08 12:33:18 +0530

    after ota .63

--- src/services/api/api-constants.ts
+++ src/services/api/api-constants.ts
@@ -62,7 +62,7 @@ const ENVURLSet: ENVURLs = {
 //     : Environment.production
 
 const currentENV = Environment.production;
-const ota=".62"
+const ota=".63"
 const currentVersion = Application.nativeApplicationVersion+ota
 
 
